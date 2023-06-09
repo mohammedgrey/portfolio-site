20a90851e4a7c6cd67b4fa141f6505d6ebb9c842
mohammedgrey <mohammeddsaadd@yahoo.com>
2023-06-09 10:52:20 +0300

    fix: remove the extra div in the projects page

--- app/(app)/projects/page.tsx
+++ app/(app)/projects/page.tsx
@@ -2,9 +2,5 @@ import Projects from "./components/Projects/Projects";
 import { projects } from "./data/projects";
 
 export default function ProjectsPage() {
-  return (
-    <div>
-      <Projects projects={projects} />
-    </div>
-  );
+  return <Projects projects={projects} />;
 }
