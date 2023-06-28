4200a9cac129ca208a60a088a79dfc9ee784b71d
mohammedgrey <mohammeddsaadd@yahoo.com>
2023-06-28 20:53:00 +0300

    update: project images

--- app/(app)/projects/data/projects.ts
+++ app/(app)/projects/data/projects.ts
@@ -7,8 +7,7 @@ export const projects: ProjectType[] = [
     title: "Look me up",
     type: "Search Engine",
     link: "https://lookkmeup.web.app/",
-    image:
-      "https://res.cloudinary.com/dxome9kh1/image/upload/v1623137489/Portfolio/lookmeup_le7lcj.png",
+    image: "/assets/images/projects/lookmeup/cover.png",
     details: {
       brief:
         "A google-like mini search engine application with 5k crawled and indexed pages. Worked with 3 other members on both the backend and frontend.",
@@ -25,8 +24,7 @@ export const projects: ProjectType[] = [
     title: "IzI Handmade",
     type: "Online Store",
     link: "https://izihandmade.web.app/",
-    image:
-      "https://res.cloudinary.com/dxome9kh1/image/upload/v1623137468/Portfolio/izi_eywaks.png",
+    image: "/assets/images/projects/izi/cover.png",
     details: {
       brief:
         "An online store project. Worked on this project solo. Firebase was used for authentication (With Facebook and Google.) There are user roles and permissions implemented on both the server and client side, as there is an admin who can add new products and change prices…etc. ",
@@ -41,8 +39,7 @@ export const projects: ProjectType[] = [
     id: uuidv4(),
     title: "EgySchools",
     type: "Schools System",
-    image:
-      "https://res.cloudinary.com/dxome9kh1/image/upload/v1623331280/Portfolio/EgySchoolsCarousel%20/1_hysikz.png",
+    image: "/assets/images/projects/schools/cover.png",
     details: {
       brief:
         "This is the project I worked on during my internship period with Datatec last summer. The project is mainly about creating a system that notifies students about their bus arrival time, upcoming exams and assignments due dates. It is also for parents to be able to track down their children’s progress in the school. And there is an admin panel for the staff and teachers to add bus schedules, due dates, and pretty much manage any interaction done with the system like doing the basic CRUD operations to the users/students/stations/routes/trips/drivers…etc. I have personally taken part in developing both the UI of the student-parent/driver application and the admin panel using Flutter and react.JS respectively. I no longer have access to the repository, as my internship period is over but I am providing a carousel for the pages I have worked on.",
@@ -58,29 +55,12 @@ export const projects: ProjectType[] = [
       PDF: "/assets/pdfs/Datatec.pdf",
     },
   },
-  {
-    id: uuidv4(),
-    title: "Create and Share",
-    type: "Social Platform",
-    link: "https://createandshare.netlify.app/",
-    image:
-      "https://res.cloudinary.com/dxome9kh1/image/upload/v1623137467/Portfolio/cands_mfrs3m.png",
-    details: {
-      brief:
-        "A typical social platform. Worked on this project solo. It has a web version made with React.JS and Mobile version with React Native. All the basic CRUD operations are included like showing the user profile, adding and changing pictures and videos, following other users...etc on both the frontend and backend. As well as authentication and authorization implemented from scratch from the server side.",
-      techs: ["NodeJS", "Express", "ReactJS", "React Native", "MongoDB"],
-      git: {
-        client: "https://github.com/mohammedgrey/createandshare",
-        server: "https://github.com/mohammedgrey/createandshareserver",
-      },
-    },
-  },
+
   {
     id: uuidv4(),
     title: "Spotify Clone",
     type: "Music App",
-    image:
-      "https://res.cloudinary.com/dxome9kh1/image/upload/v1623137461/Portfolio/spotify_o0hq4e.png",
+    image: "/assets/images/projects/spotify/cover.png",
     details: {
       brief:
         "A Spotify clone project. Worked on the client side of this project along with 4 other members. My tasks included things like redux management, search page, user page, help page…etc.",
@@ -94,7 +74,7 @@ export const projects: ProjectType[] = [
     id: uuidv4(),
     title: "Santa Game",
     type: "Game Engine",
-    image: "/assets/images/santa.jpg",
+    image: "/assets/images/projects/santa/cover.png",
     link: "https://drive.google.com/file/d/1RSvIiQUV_hW8EbJwNwemhZwKQCLNBjBw/view?usp=sharing",
     details: {
       brief:
@@ -109,7 +89,7 @@ export const projects: ProjectType[] = [
     id: uuidv4(),
     title: "Grades Autofiller",
     type: "Image Processing",
-    image: "/assets/images/Grader.jpg",
+    image: "/assets/images/projects/sheetgrader/cover.png",
     link: "https://sheetgrader.web.app/",
     details: {
       brief:
@@ -126,7 +106,7 @@ export const projects: ProjectType[] = [
     id: uuidv4(),
     title: "Harvard Processor",
     type: "Processor",
-    image: "/assets/images/hardware.jpg",
+    image: "/assets/images/projects/processor/cover.png",
     containImage: true,
     details: {
       brief:
@@ -142,7 +122,7 @@ export const projects: ProjectType[] = [
     id: uuidv4(),
     title: "Font Classifier",
     type: "Machine Learning",
-    image: "/assets/images/FontClassifier.png",
+    image: "/assets/images/projects/fontclassifier/cover.png",
     link: "https://drive.google.com/file/d/1D5ahomRW4M7fShPb3K_HgJEOPcoD0TtX/view?usp=sharing",
     details: {
       brief:
@@ -153,9 +133,9 @@ export const projects: ProjectType[] = [
   },
   {
     id: uuidv4(),
-    title: "Selective Repeat Protocol",
+    title: "SRP",
     type: "Networks",
-    image: "/assets/images/Networks.png",
+    image: "/assets/images/projects/networks/cover.png",
     details: {
       brief: "Selective repeat protocol using omnet++",
       techs: ["omnet++", "C++"],
