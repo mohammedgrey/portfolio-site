ff75abeaeff92dc020d101854e9df18a3763214a
Mohammed Saad <mohammeddsaadd@yahoo.com>
2024-11-01 09:50:21 +0300

    feat: update og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,30 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import { Metadata } from "next";
 import GoogleAnalytics from "@/components/GoogleAnalytics/GoogleAnalytics";
+import images from "./configs/images";
+
+const title = "Mohammed Dawood";
+const description =
+  "I am a software engineer who is passionate about creating aesthetically pleasing and fully functioning websites from the inside out.";
 
 export const metadata: Metadata = {
-  title: "Mohammed Dawood",
-  description:
-    "I am a software engineer who is passionate about creating aesthetically pleasing and fully functioning websites from the inside out.",
+  title,
+  description,
   keywords:
     "software, developer, frontend, engineering, backend, fullstack, websites, development, coding, agile, javascript, typescript, react, native, node, express, mongodb, firebase, sql, git, vue, web, mohammed, dawood, muhammad, mohamed",
+
+  openGraph: {
+    title,
+    description,
+    images: [
+      {
+        url: images.home.frame.cover,
+        width: 1201,
+        height: 1201,
+        alt: "Mohammed Dawood's profile",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
